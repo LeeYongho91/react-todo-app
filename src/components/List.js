18a7e05c9e16fc734006b287ba76fb9f746d4576
LeeYongho91 <skyks4848@naver.com>
2024-01-09 00:15:32 +0900

    test

--- src/components/List.js
+++ src/components/List.js
@@ -22,21 +22,21 @@ const handleClick = (id) => {
 }
 
   return (
-      <div
-        key={id}
-        {...provided.draggableProps}
-        ref={provided.innerRef}
-        {...provided.dragHandleProps}
-        className={`${
-          snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`} 
-      >
+    <div
+      key={id}
+      {...provided.draggableProps}
+      ref={provided.innerRef}
+      {...provided.dragHandleProps}
+      className={`${
+        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`} 
+    >
+          <div className='items-center'>
+          <input type='checkbox' defaultChecked={false} onChange={() => handleCompleteChange(id)} className='mr-2' />
+            <span className={completed ? "line-through" : undefined}>{title}</span>
+          </div>
             <div className='items-center'>
-            <input type='checkbox' defaultChecked={false} onChange={() => handleCompleteChange(id)} className='mr-2' />
-              <span className={completed ? "line-through" : undefined}>{title}</span>
-            </div>
-              <div className='items-center'>
-              <button className='px-4 py-2' onClick={() => handleClick(id)}>x</button>
-              </div>     
-            </div>
+            <button className='px-4 py-2' onClick={() => handleClick(id)}>x</button>
+            </div>     
+          </div>
   )
 }
